b103dad9b2f882c584f9c5e6b5b7916dde3246e7
Udhay-Adithya <udhayxd@gmail.com>
2025-04-15 15:16:30 +0530

    feat: add form and response tables with upgrade and downgrade functions

--- app/db/base.py
+++ app/db/base.py
@@ -4,5 +4,5 @@
 from app.db.base_class import Base
 from app.models.user import User  # noqa F401 - Import user model
 # Import other models here as you create them
-# from app.models.form import Form # noqa F401
-# from app.models.response import Response # noqa F401
+from app.models.form import Form # noqa F401
+from app.models.response import Response # noqa F401
